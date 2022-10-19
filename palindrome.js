a5b8f2eef5c98cb499dacbbc376ba9abacc635b2
jack <jackayron5@gmail.com>
2022-10-19 15:46:52 +0530

    added_primeand_palindrome

---
+++ palindrome.js
@@ -0,0 +1,9 @@
+let str = "madam";
+bag =" ";
+
+for(let i = 0; i<=str.length-1;i++){
+    if(str[i]==str[0]){
+bag+=str[i];
+    }
+}
+console.log(bag);
---
+++ prime.js
@@ -0,0 +1,14 @@
+
+
+let count = 0;
+for (let i = 1; i <= num; i++) {
+  if (num % i == 0) {
+    count++;
+  }
+}
+
+if (count == 1) {
+  console.log("Yes");
+} else {
+  console.log("No");
+}
